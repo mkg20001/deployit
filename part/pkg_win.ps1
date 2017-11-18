84e48d14dcad8127fea2d35033ff162bfd889448
mkg20001 <mkg20001@gmail.com>
2017-11-18 12:31:09 +0100

    Remove php

--- part/pkg_win.ps1
+++ part/pkg_win.ps1
@@ -1,5 +1,5 @@
 #all the apps
-choco install atom nodejs git filezilla go-ipfs firefox php make ruby -y --accept-licenses
+choco install atom nodejs git filezilla go-ipfs firefox make ruby -y --accept-licenses
 
 #keep confirmed
 choco feature enable -n allowGlobalConfirmation
