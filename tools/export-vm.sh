dcf8606e5c2ddf11cdeab945c0e2609a2ff6ff14
mkg20001 <mkg20001@gmail.com>
2018-03-24 13:05:43 +0100

    Fix file type

--- tools/export-vm.sh
+++ tools/export-vm.sh
@@ -3,5 +3,5 @@
 set -ex
 
 VBoxManage export DevVM2 -o devvm2.installed.ova --options nomacs
-tar cvf devvm2.tar.bz2.ova -I pbzip2 devvm2.installed.ova
+tar cvf devvm2.ova.tar.bz2 -I pbzip2 devvm2.installed.ova
 rm devvm2.installed.ova
