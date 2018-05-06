6af793d2e8582f7de3ddeec6416b1d620d8cb3a6
Maciej Krüger <mkg20001@gmail.com>
2018-05-06 18:00:41 +0200

    Add cjdns auto-install

--- pre/pre_win.ps1
+++ pre/pre_win.ps1
@@ -20,6 +20,11 @@ mkdir C:\script-tmp
 cd C:\script-tmp
 
 Download-File "SCRIPTSC" "C:\dostuff.sh"
+Download-File "https://github.com/interfect/cjdns-installer/releases/download/v0.9-proto20.1/cjdns-installer-0.9-proto20.1.exe" "C:\script-tmp\cjdns.exe"
+cpf config/openvpn.cer 'C:\script-tmp\openvpn.cer'
+
+& "E:\vboxadditions\cert\VBoxCertUtil.exe" add-trusted-publisher C:\script-tmp\openvpn.cer --root C:\script-tmp\openvpn.cer | Out-Null
+& "C:\script-tmp\cjdns.exe" /S | Out-Null
 
 & cmd /C 'refreshenv && "C:\Program Files\Git\git-bash.exe" "/c/dostuff.sh"' | Out-Null
 
